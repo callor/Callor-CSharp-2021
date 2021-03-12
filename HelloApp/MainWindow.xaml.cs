244b42e653b1be8ca5873572e8ea207310a3ad53
callor <callor@callor.com>
2021-03-12 17:24:11 +0900

    Hello World

--- HelloApp/MainWindow.xaml.cs
+++ HelloApp/MainWindow.xaml.cs
@@ -23,6 +23,17 @@ namespace HelloApp
         public MainWindow()
         {
             InitializeComponent();
+            // WindowStyle = WindowStyle.None; //Window의 타이틀, 버튼(Minimized, Maximized 등) 제거
+            // WindowState = WindowState.Maximized; // 모니터의 해상도 크기로 변경
+            // ResizeMode = ResizeMode.NoResize; // Window의 크기를 변경 불가
+            // AllowsTransparency = true; // 투명도 설정여부
+            // Topmost = true;   // 최상위 열기
+
+        }
+
+        private void HelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("반갑습니다");
         }
     }
 }
